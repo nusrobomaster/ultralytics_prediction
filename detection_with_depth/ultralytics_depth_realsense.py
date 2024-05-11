81ba2f78d54e055b61ec5eb9f123925d37b43d15
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-05-12 00:45:32 +0800

    Add global constant for cleaner reference

--- detection_with_depth/ultralytics_depth_realsense.py
+++ detection_with_depth/ultralytics_depth_realsense.py
@@ -3,14 +3,20 @@ import time
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+from spatial_location_calculator import SpatialLocationCalculator
+
+IMAGE_WIDTH = 640
+IMAGE_HEIGHT = 480
+
+spatial_location_calculator = SpatialLocationCalculator(IMAGE_WIDTH, IMAGE_HEIGHT)
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
 
 # Start streaming
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, IMAGE_WIDTH, IMAGE_HEIGHT, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, IMAGE_WIDTH, IMAGE_HEIGHT, rs.format.z16, 30)
 
 profile = pipeline.start(config)
 

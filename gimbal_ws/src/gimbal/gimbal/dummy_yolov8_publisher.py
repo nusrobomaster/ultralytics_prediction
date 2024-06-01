cc6315b852bb3fc18a5b71c0b1aeb70fd4ea31b1
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-06-01 17:04:17 +0800

    Add yolov8 pubsub

---
+++ gimbal_ws/src/gimbal/gimbal/dummy_yolov8_publisher.py
@@ -0,0 +1,66 @@
+
+import rclpy
+from rclpy.node import Node
+from vision_msgs.msg import Detection2DArray, Detection2D, ObjectHypothesisWithPose
+from std_msgs.msg import Header
+from object_detector import ObjectDetector
+from camera import Camera
+
+
+class DummyYOLOv8DetectionPublisher(Node):
+    def __init__(self):
+        super().__init__('yolov8_detection_publisher')
+        self.publisher_ = self.create_publisher(Detection2DArray, 'detections_output', 10)
+        self.object_detector = ObjectDetector('yolov8n.pt')
+        self.timer = self.create_timer(0.05, self.timer_callback)
+        self.camera = Camera(640, 480)
+
+    def timer_callback(self):
+        color_image = self.get_image()
+
+        # Get YOLOv8 results
+        results = self.object_detector.detect_objects(color_image)
+
+        # Create and populate the Detection2DArray message
+        detections_msg = Detection2DArray()
+        detections_msg.header = self.create_header()
+
+        for box in results.boxes:
+            detection = Detection2D()
+            center_x, center_y, width, height = box.xywh[0] # Access the bounding box coordinates
+
+            detection.bbox.center.position.x = float(center_x)
+            detection.bbox.center.position.y = float(center_y)
+            detection.bbox.size_x = float(width)
+            detection.bbox.size_y = float(height)
+            
+            hypothesis = ObjectHypothesisWithPose()
+            # print(hypothesis)
+            hypothesis.hypothesis.class_id = str(int(box.cls[0].item()))  # Class ID
+            hypothesis.hypothesis.score = float(box.conf[0].item())  # Confidence score
+            detection.results.append(hypothesis)
+
+            detections_msg.detections.append(detection)
+
+        self.publisher_.publish(detections_msg)
+        # self.get_logger().info(detections_msg)
+
+    def create_header(self):
+        header = Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = 'camera_frame'
+        return header
+
+    def get_image(self):
+        depth_image, color_image = self.camera.get_frames()
+        return color_image
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DummyYOLOv8DetectionPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ gimbal_ws/src/gimbal/gimbal/yolov8_detection_subscriber.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from vision_msgs.msg import Detection2DArray
+from ultralytics.engine.results import Results
+import numpy as np
+
+class Yolov8DetectionSubscriber(Node):
+    def __init__(self):
+        super().__init__('detection_subscriber')
+        self.subscription = self.create_subscription(
+            Detection2DArray,
+            'detections_output',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.results = None
+
+    def listener_callback(self, msg):
+        self.results = self.convert_detections(msg)
+        self.get_logger().info(f'Received {len(self.results.cls)} detections')
+
+    def convert_detections(self, msg):
+        boxes = []
+        confidences = []
+        class_ids = []
+        for detection in msg.detections:
+            x_center = detection.bbox.center.position.x
+            y_center = detection.bbox.center.position.y
+            width = detection.bbox.size_x
+            height = detection.bbox.size_y
+            x_min = x_center - width / 2
+            y_min = y_center - height / 2
+            x_max = x_center + width / 2
+            y_max = y_center + height / 2
+            boxes.append([x_min, y_min, x_max, y_max])
+            class_ids.append(int(detection.results[0].hypothesis.class_id))
+            confidences.append(detection.results[0].hypothesis.score)
+
+        boxes = np.array(boxes)
+        confidences = np.array(confidences)
+        class_ids = np.array(class_ids)
+
+        results = Results(
+            orig_img=np.zeros((self.image_height, self.image_width, 3), dtype=np.uint8),
+            path='',
+            names={i: f'class_{i}' for i in range(len(class_ids))},
+            boxes=boxes,
+            conf=confidences,
+            cls=class_ids
+        )
+        
+        return results

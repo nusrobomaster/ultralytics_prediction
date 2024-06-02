f72726c43f6163b5db713e04da7ce0862a8c6ddd
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-06-02 21:50:40 +0800

    Add publisher for rgb data

--- gimbal_ws/src/gimbal/gimbal/dummy_yolov8_publisher.py
+++ gimbal_ws/src/gimbal/gimbal/dummy_yolov8_publisher.py
@@ -5,19 +5,26 @@ from vision_msgs.msg import Detection2DArray, Detection2D, ObjectHypothesisWithP
 from std_msgs.msg import Header
 from object_detector import ObjectDetector
 from camera import Camera
-
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 class DummyYOLOv8DetectionPublisher(Node):
     def __init__(self):
         super().__init__('yolov8_detection_publisher')
-        self.publisher_ = self.create_publisher(Detection2DArray, 'detections_output', 10)
-        self.object_detector = ObjectDetector('yolov8n.pt')
+        self.bridge = CvBridge()
+
+        self.detection_publisher = self.create_publisher(Detection2DArray, 'detections_output', 10)
+        self.image_publisher = self.create_publisher(Image, 'rgb_footage', 10)        
         self.timer = self.create_timer(0.05, self.timer_callback)
+
+        self.object_detector = ObjectDetector('yolov8n.pt')
         self.camera = Camera(640, 480)
 
     def timer_callback(self):
-        color_image = self.get_image()
-
+        depth_image, color_image = self.camera.get_frames()
+        if depth_image is None or color_image is None:
+            return
+        
         # Get YOLOv8 results
         results = self.object_detector.detect_objects(color_image)
 
@@ -35,14 +42,15 @@ class DummyYOLOv8DetectionPublisher(Node):
             detection.bbox.size_y = float(height)
             
             hypothesis = ObjectHypothesisWithPose()
-            # print(hypothesis)
             hypothesis.hypothesis.class_id = str(int(box.cls[0].item()))  # Class ID
             hypothesis.hypothesis.score = float(box.conf[0].item())  # Confidence score
             detection.results.append(hypothesis)
 
             detections_msg.detections.append(detection)
 
-        self.publisher_.publish(detections_msg)
+        self.detection_publisher.publish(detections_msg)
+        self.image_publisher.publish(self.bridge.cv2_to_imgmsg(color_image, "bgr8"))
+
         self.get_logger().info('Identified %d armour plates' % len(detections_msg.detections))
 
     def create_header(self):
@@ -50,11 +58,7 @@ class DummyYOLOv8DetectionPublisher(Node):
         header.stamp = self.get_clock().now().to_msg()
         header.frame_id = 'camera_frame'
         return header
-
-    def get_image(self):
-        depth_image, color_image = self.camera.get_frames()
-        return color_image
-
+    
 def main(args=None):
     rclpy.init(args=args)
     node = DummyYOLOv8DetectionPublisher()

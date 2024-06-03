4b5900f37abb74efec1f9d1870c2f17124c8209e
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-06-03 16:13:45 +0800

    Add back camera node

--- gimbal_ws/src/gimbal/gimbal/main.py
+++ gimbal_ws/src/gimbal/gimbal/main.py
@@ -16,21 +16,27 @@ class Main(Node):
 
         # detection process objects for front and back cameras
         front_camera_detection_processor = DetectionProcessor(gimbal, spatial_calculator, 'front_camera')
+        # back_camera_detection_processor = DetectionProcessor(gimbal, spatial_calculator, 'back_camera')
         
         # running rosnodes using multithreading
         executor = rclpy.executors.MultiThreadedExecutor()
         executor.add_node(gimbal)
         executor.add_node(front_camera_detection_processor)
-        executor.add_node(front_camera_detection_processor.yolov8_detector)
+        # executor.add_node(back_camera_detection_processor)
 
         while rclpy.ok():
             rclpy.spin_once(front_camera_detection_processor, timeout_sec=0.02)
+            # rclpy.spin_once(back_camera_detection_processor, timeout_sec=0.02)
 
             # visualise detection results
             if front_camera_detection_processor.get_annotated_frame() is not None:
                 cv2.imshow("Front Camera", front_camera_detection_processor.get_annotated_frame())
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            # if back_camera_detection_processor.get_annotated_frame() is not None:
+            #     cv2.imshow("Back Camera", back_camera_detection_processor.get_annotated_frame())
+            #     if cv2.waitKey(1) & 0xFF == ord('q'):
+            #         break
 
 def main(args=None):
     rclpy.init(args=args)
